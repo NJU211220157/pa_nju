0bbf5b043c2b431ff24d213604d5e59cd9cc4b30
tracer-pa-public <tracer@njuics.org>
2022-09-18 20:36:50 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:36:50 up 60 days, 12:03,  1 user,  load average: 2.24, 2.37, 2.36 > unitime 1663504610001 > b409a10321c89236eb4d55e8c518a2889c0cb556

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -79,6 +79,7 @@ void set_CF_adc(uint32_t res, uint32_t src, size_t data_size){
     else 
         cpu.eflags.CF = res < src;
 }
+//bug
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
