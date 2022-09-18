5cfa166b58012809cd315d32c1e781116a9ded32
tracer-pa-public <tracer@njuics.org>
2022-09-18 16:09:50 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:09:50 up 60 days,  7:36,  1 user,  load average: 3.11, 2.75, 2.51 > unitime > 8ac6e8cc5220cb032c096fb9fcf549479872c5b3

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -74,7 +74,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
     set_OF_add(res,src,dest,data_size);
     set_SF(res,data_size);
     set_PF(res);
-	return __ref_alu_sub&(0xFFFFFFFF>>(32-data_size));//32位无符号加法
+	return res&(0xFFFFFFFF>>(32-data_size));//32位无符号加法
 #endif
 }
 
