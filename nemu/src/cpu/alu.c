64d01a6ac39388c9987c875fb11dfb7092c19e5a
tracer-pa-public <tracer@njuics.org>
2022-09-18 20:56:40 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:56:40 up 60 days, 12:23,  1 user,  load average: 2.41, 2.26, 2.27 > unitime > 69d36446fc5c108b2a4fecbc8ec65065c7555edb

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -70,7 +70,6 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	return res&(0xFFFFFFFF>>(32-data_size));//32位无符号加法
 #endif
 }
-
 void set_CF_adc(uint32_t res, uint32_t src, size_t data_size){
     res = sign_ext(res&(0xFFFFFFFF>>(32-data_size)),data_size);
     src = sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
@@ -95,7 +94,6 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 	return res&(0xFFFFFFFF>>(32-data_size));
 #endif
 }
-
 void set_CF_sub(uint32_t dest, uint32_t src, size_t data_size){
     dest = sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
     src = sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
@@ -168,7 +166,6 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 	set_CF_sbb(dest,src,data_size);
 	set_OF_sub(res,dest,src,data_size);
 	return res & (0xFFFFFFFF>>(32-data_size));
-	return 0;
 #endif
 }
 
@@ -251,10 +248,14 @@ uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_and(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+	uint32_t result;
+	result = src & dest;
+	set_ZF(result);
+	set_SF(result);
+	set_PF(result);
+	cpu.eflags.OF=0;
+	cpu.eflags.CF=0;
+	return result;
 #endif
 }
 
