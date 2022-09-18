04590dfa786c00335ed59d703ce3f3a11a2fbda5
tracer-pa-public <tracer@njuics.org>
2022-09-18 16:32:17 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:32:17 up 60 days,  7:58,  1 user,  load average: 2.58, 2.53, 2.48 > unitime > d7f224494fc5c956df7ebd34b8b5eb53fc57068

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -74,8 +74,8 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 void set_CF_adc(uint32_t res, uint32_t src, size_t data_size){
     res = sign_ext(res&(0xFFFFFFFF>>(32-data_size)),data_size);
     src = sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
-    if(cpu.eflags.CF==1)
-        cpu.eflags.CF = res < src + 1;
+    if(cpu.eflags.CF == 1)
+        cpu.eflags.CF = res <= src
     else 
         cpu.eflags.CF = res < src;
 }
