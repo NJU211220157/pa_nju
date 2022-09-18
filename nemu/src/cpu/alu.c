c83f09be736b6774d08660dd5e4ef09ba8857027
tracer-pa-public <tracer@njuics.org>
2022-09-18 16:27:56 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:27:56 up 60 days,  7:54,  1 user,  load average: 2.11, 2.35, 2.42 > unitime 1663489676221 > b3280d34192d74ccff0835d4f9e4fc007bd591c4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -71,6 +71,38 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 #endif
 }
 
+void set_CF_adc(uint32_t res, uint32_t src, size_t data_size){
+    res = sign_ext(res&(0xFFFFFFFF>>(32-data_size)),data_size);
+    src = sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
+    cpu.eflags.CF = res<(src+cpu.eflags.CF);
+}
+void set_OF_adc(uint32_t res, uint32_t src, uint32_t dest,size_t data_size){
+    switch(data_size)
+    {
+        case 8:{
+            result = sign_ext(result & 0xFF,8);//截取低八位
+            src = sign_ext(src & 0xFF,8);
+            dest = sign_ext(dest & 0xFF,8);
+            break;
+        }
+        case 16:{
+            result = sign_ext(result & 0xFFFF,16);
+            src = sign_ext(src & 0xFFFF,16);
+            dest = sign_ext(dest & 0xFFFF,16);
+            break;
+        }
+        default:break;
+    }
+    if(sign(src)==sign(dest))
+    {
+        if(sign(src)!=sign(result))
+            cpu.eflags.OF = 1;
+        else 
+            cpu.eflags.OF = 0;
+    }
+    else
+        cpu.eflags.OF = 0;//正负相加一定不会溢出
+}
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
@@ -78,9 +110,9 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #else
 	uint32_t res=0;
 	res = src + dest +cpu.eflags.CF;
-	//set_CF_add(res,src,data_size);
+	set_CF_adc(res,src,data_size);
     set_ZF(res,data_size);
-    //set_OF_add(res,src,dest,data_size);
+    set_OF_add(res,src,dest,data_size);
     set_SF(res,data_size);
     set_PF(res);
 	return 0;
