3066f5af5ed255040debda07cb8adf01ac41fdc1
tracer-pa-public <tracer@njuics.org>
2022-09-23 15:03:01 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:03:01 up 65 days,  6:29,  1 user,  load average: 1.18, 1.24, 1.14 > unitime > e3641b5e4f0cc45ce86a51fe6070d362181d3b2c

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -140,7 +140,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 		return b;
 	}
 
-	if (fa.exponent > fb.exponent)
+	if (fa.exponent > fb.exponent)//fb是阶数更大的数
 	{
 		fa.val = b;
 		fb.val = a;
@@ -158,10 +158,7 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	uint32_t shift = 0;
 
 	/* TODO: shift = ? */
-	printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	assert(shift >= 0);
+	shift = fb.exponent-fa.exponent;
 
 	sig_a = (sig_a << 3); // guard, round, sticky
 	sig_b = (sig_b << 3);
