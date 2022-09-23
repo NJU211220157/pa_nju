c429e47fc88674c3831eb063de6bfdcdd4a6e17c
tracer-pa-public <tracer@njuics.org>
2022-09-23 20:34:13 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:34:13 up 65 days, 12:00,  1 user,  load average: 1.56, 1.27, 1.17 > unitime > 1fadd91429614569e46340492031ec9010db9b82

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -297,7 +297,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-    exp_res = fa.exponent + fb.exponent- 0x7F; // 1 1 --> -252  2-> -125
+    exp_res = fa.exponent + fb.exponent- 0x7F-23; // 1 1 --> -252  2-> -125
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
 
