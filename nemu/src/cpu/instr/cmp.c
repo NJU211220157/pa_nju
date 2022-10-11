36a3f3a10a377d294214b99ccc3f2e7feadc464a
tracer-pa-public <tracer@njuics.org>
2022-10-11 17:17:56 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:17:56 up 83 days,  8:44,  1 user,  load average: 2.35, 2.27, 2.17 > unitime > ee0f414f61f94cdc2ccc252b85c37453492e551b

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -13,33 +13,33 @@ static void instr_execute_2op(){
 }
 //make_instr_impl_2op(mov, r, rm, b)
 
-/*make_instr_impl_2op(cmp,rm,r,b);
+make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,r,rm,v);
 make_instr_impl_2op(cmp,i,a,b);
-make_instr_impl_2op(cmp,i,a,v);*/
+make_instr_impl_2op(cmp,i,a,v);
 
-make_instr_func(cmp_rm2r_b)
-{
-    OPERAND rm,imm;
-    imm.data_size=rm.data_size=8;
+// make_instr_func(cmp_rm2r_b)
+// {
+//     OPERAND rm,imm;
+//     imm.data_size=rm.data_size=8;
     
-    int len=1;
-    len += modrm_rm(eip+1,&rm);
-    imm.type=OPR_IMM;
-    imm.sreg=SREG_CS;
-    imm.addr=eip+len;
-    operand_read(&opr_src);
-    long res=rm.val-imm.val;
-    if(res==0){
-        cpu.eflags.ZF=0;
-    }
-    cpu.eflags.SF=res>=0?0:1;
-    operand_write(&opr_dest);
+//     int len=1;
+//     len += modrm_rm(eip+1,&rm);
+//     imm.type=OPR_IMM;
+//     imm.sreg=SREG_CS;
+//     imm.addr=eip+len;
+//     operand_read(&opr_src);
+//     long res=rm.val-imm.val;
+//     if(res==0){
+//         cpu.eflags.ZF=0;
+//     }
+//     cpu.eflags.SF=res>=0?0:1;
+//     operand_write(&opr_dest);
     
-    return len+1;
-}
+//     return len+1;
+// }
 
 
 
