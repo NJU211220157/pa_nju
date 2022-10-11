fbfacdda9e43e4fe674e0b4df90560df19aa4c81
tracer-pa-public <tracer@njuics.org>
2022-10-11 19:48:19 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:48:19 up 83 days, 11:14,  1 user,  load average: 2.61, 2.49, 2.43 > unitime > 781a9549cfc81625e1fcc46d25a03b0511bd15d

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -13,22 +13,8 @@ make_instr_impl_2op(cmp,rm,r,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,r,rm,v);
 make_instr_impl_2op(cmp,i,a,b);
-//make_instr_impl_2op(cmp,i,a,v);
+make_instr_impl_2op(cmp,i,a,v);
 
-make_instr_func(cmp_i2a_v){
-    OPERAND rm,imm;
-    
-    imm.data_size=rm.data_size=data_size;
-    int len=1;
-    //没有modR/M字节
-    imm.type=OPR_IMM;
-    imm.type=SREG_CS;
-    imm.addr=eip+1;
-    operand_read(&imm);
-    operand_read(&rm);
-    alu_sub(opr_src.val,opr_dest.val,data_size);
-    return len+2;
-}
 
 // make_instr_func(cmp_rm2r_b)
 // {
