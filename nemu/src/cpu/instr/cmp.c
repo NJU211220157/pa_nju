aaca7139fde61b26b8fae15aabe0dc6697471335
tracer-pa-public <tracer@njuics.org>
2022-10-11 19:02:23 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:02:23 up 83 days, 10:29,  1 user,  load average: 2.04, 2.12, 2.16 > unitime > 1dbd55c95c9d453945ba8528d9a0c1c83f3990fd

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -12,8 +12,8 @@ make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,r,rm,v);
-make_instr_impl_2op(cmp,i,a,b);
-make_instr_impl_2op(cmp,i,a,v);
+make_instr_impl_2op(cmp,i,r,b);
+make_instr_impl_2op(cmp,i,r,v);
 
 // make_instr_func(cmp_rm2r_b)
 // {
