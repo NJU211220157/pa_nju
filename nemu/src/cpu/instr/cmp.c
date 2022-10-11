c21750a096ca58f122a87ad9b39aad754989f55d
tracer-pa-public <tracer@njuics.org>
2022-10-11 19:46:51 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:46:51 up 83 days, 11:13,  1 user,  load average: 2.56, 2.44, 2.41 > unitime > d72e7560ed6715bc2a49320b1b7522bcd79b2130

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -6,25 +6,30 @@ Put the implementations of `cmp' instructions here.
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    alu_sub(opr_src.val,opr_dest.val,8);
+    alu_sub(opr_src.val,opr_dest.val,opr_src.data_size);
 }
 make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,r,rm,v);
 make_instr_impl_2op(cmp,i,a,b);
+make_instr_impl_2op(cmp,i,a,v);
+/*
 make_instr_func(cmp_i2a_v){
     OPERAND rm,imm;
     
     imm.data_size=rm.data_size=data_size;
     int len=1;
-    len += modrm_rm(eip+1,&rm);
+    //没有modR/M字节
     imm.type=OPR_IMM;
     imm.type=SREG_CS;
     imm.addr=eip+1;
-    int len=1;
+    operand_read(&imm);
+    operand_read(&rm);
+    alu_sub(opr_src.val,opr_dest.val,data_size);
+    return len+2;
 }
-
+*/
 // make_instr_func(cmp_rm2r_b)
 // {
 //     OPERAND rm,imm;
