b179943ab0e004fd66e0045eb243636fad14ca49
tracer-pa-public <tracer@njuics.org>
2022-10-11 19:51:45 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:51:45 up 83 days, 11:18,  1 user,  load average: 2.20, 2.42, 2.42 > unitime > 52960c1a6690859d345ad33756be79a19a1c918c

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -1,13 +1,12 @@
 #include "cpu/instr.h"
 #include "cpu/alu.h"
-#include<iostream>
 /*
 Put the implementations of `cmp' instructions here.
 */
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    std::cout<<alu_sub(opr_src.val,opr_dest.val,opr_src.data_size);
+    alu_sub(opr_src.val,opr_dest.val,opr_src.data_size);
 }
 make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
