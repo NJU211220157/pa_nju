df1f37da27cdeef863e7ae55045950e8cd40d9b4
tracer-pa-public <tracer@njuics.org>
2022-10-12 11:13:52 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:13:52 up 84 days,  2:40,  1 user,  load average: 2.73, 2.50, 2.40 > unitime > 7123223d2f48ce22c50d195099598a4eb2aa8891

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -12,8 +12,8 @@ make_instr_func(call_near){
     rm.type=OPR_MEM;
     rm.addr=cpu.gpr[4].val;
     rm.data_size=data_size;
-    //rm.val=cpu.eip+ 1 + (data_size/8);//important
-    rm.val=cpu.eip;
+    rm.val=cpu.eip+ 1 + (data_size/8);//important
+    //rm.val=cpu.eip;
     
     operand_write(&rm);
     
