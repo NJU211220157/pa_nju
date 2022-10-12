ac31a1b70de8a3c8411cd65d749a71bac41e5eb5
tracer-pa-public <tracer@njuics.org>
2022-10-12 16:53:19 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:53:19 up 84 days,  8:19,  1 user,  load average: 2.93, 2.80, 2.47 > unitime > 9fd5ecdf4f003b576495773458a0a4b33415c1c2

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,24 +2,24 @@
 /*
 Put the implementations of `inc' instructions here.
 */
-// static void instr_execute_1op()
-// {
-//     operand_read(&opr_src);
-//     opr_src.val=opr_src.val+1;
-//     operand_write(&opr_src);
-// }
-// make_instr_impl_1op(inc,rm,v);
-
-make_instr_func(inc_rm_v)
+static void instr_execute_1op()
 {
-    // OPERAND rm;
-    // rm.data_size=data_size;
-    // int len=1;
-    // len += modrm(eip+1,&rm);//寻址
-    // operand_read(&rm);
-    
-    // rm.val+=1;
-    // operand_write(&rm);
-    
-    return 3;
+    operand_read(&opr_src);
+    opr_src.val=alu_add(0x1,opr_src.val,data_size);
+    operand_write(&opr_src);
 }
+make_instr_impl_1op(inc,rm,v);
+
+// make_instr_func(inc_rm_v)
+// {
+//     // OPERAND rm;
+//     // rm.data_size=data_size;
+//     // int len=1;
+//     // len += modrm(eip+1,&rm);//寻址
+//     // operand_read(&rm);
+    
+//     // rm.val+=1;
+//     // operand_write(&rm);
+    
+//     return 3;
+// }
