8154d1b8df4ba0d9caec0832d3fb93d9b319ec3b
tracer-pa-public <tracer@njuics.org>
2022-10-12 16:16:57 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:16:57 up 84 days,  7:43,  1 user,  load average: 1.76, 2.04, 2.17 > unitime > 57ea2fb72459cbe972eb862eb01ecbaceac7c3df

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,3 +2,11 @@
 /*
 Put the implementations of `inc' instructions here.
 */
+
+static void instr_execute_1op()
+{
+    operand_read(&rm);
+    rm.val+=1;
+    operand_writr(&rm);
+}
+make_instr_impl_1op(inc,rm,v);
