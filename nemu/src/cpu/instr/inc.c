7d3aee93e1851530e4123f62ea943b68f58ac8cd
tracer-pa-public <tracer@njuics.org>
2022-10-12 17:34:21 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:34:21 up 84 days,  9:00,  1 user,  load average: 1.91, 2.18, 2.24 > unitime > 8a8fe79778e4c24f94443abb20589c17f3256db9

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -5,17 +5,17 @@ Put the implementations of `inc' instructions here.
 static void instr_execute_1op()
 {
     operand_read(&opr_src);
-    // operand_read(&opr_dest);
-    // opr_dest.data_size=data_size;
-    // opr_dest.val=1;
-    // opr_dest.type=opr_src.type;
+    operand_read(&opr_dest);
+    opr_dest.data_size=data_size;
+    opr_dest.val=1;
+    opr_dest.type=opr_src.type;
     
-    // uint32_t temp=cpu.eflags.CF;
+    uint32_t temp=cpu.eflags.CF;
     
-    // opr_src.val=alu_add(opr_src.val,opr_dest.val,data_size);
+    opr_dest.val=alu_add(opr_src.val,opr_dest.val,data_size);
+    opr_src.val=opr_dest.val;
     
-    // cpu.eflags.CF=temp;
-    opr_src.val=alu_add(opr_src.val,sign_ext(1,data_size),data_size);
+    cpu.eflags.CF=temp;
     
     operand_write(&opr_src);
 }
