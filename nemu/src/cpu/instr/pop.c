6aec2a9b2b3938433b7e5ad7dc839e4a27212114
tracer-pa-public <tracer@njuics.org>
2022-10-12 09:51:20 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:51:20 up 84 days,  1:17,  1 user,  load average: 2.43, 2.16, 2.20 > unitime > beb2417f64037ed7e5b08294b95a456afc1c5d20

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,3 +2,18 @@
 /*
 Put the implementations of `pop' instructions here.
 */
+static void instr_execute_1op(){
+    
+    OPERAND rm;
+    
+    rm.type=OPR_MEM;
+    rm.addr=cpu.gpr[4].val;
+    rm.data_size=data_size;
+    operand_read(&rm);
+    
+    opr_src.val=rm.val;
+    operand_write(&opr_src);
+    
+    cpu.gpr[4].val +=data_size/8;
+}
+make_instr_impl_1op(pop,r,v);
--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -2,3 +2,17 @@
 /*
 Put the implementations of `ret' instructions here.
 */
+make_instr_func(ret_near)
+{
+    OPERAND rm;
+    
+    rm.type=OPR_MEM;
+    rm.addr=cpu.gpr[4].val;
+    rm.data_size=data_size;
+    operand_read(&rm);
+    
+    cpu.eip=rm.val;
+    cpu.gpr[4].val +=data_size/8;
+    
+    return 1;
+}
