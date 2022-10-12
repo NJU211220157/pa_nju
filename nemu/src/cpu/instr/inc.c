ca3d6bfd0ef8df7cfa35905b6bcb9e9c37f628c4
tracer-pa-public <tracer@njuics.org>
2022-10-12 17:19:15 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:19:15 up 84 days,  8:45,  1 user,  load average: 2.10, 2.07, 2.23 > unitime > bc5c578d6b0b924780d9d82968eadf3f781a60f1

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -7,6 +7,7 @@ static void instr_execute_1op()
     operand_read(&opr_src);
     OPERAND rm;
     rm.addr=opr_src.addr;
+    rm.val=opr_src.val;
     operand_write(&rm);
 }
 make_instr_impl_1op(inc,rm,v);
