25e24a55873e3f8db2861f7095cf59d597586286
tracer-pa-public <tracer@njuics.org>
2022-10-12 16:33:42 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:33:42 up 84 days,  8:00,  1 user,  load average: 2.33, 2.33, 2.27 > unitime > f247dd1867eb5a4644643bbd22a6b58b49f42786

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -7,7 +7,7 @@ static void instr_execute_1op()
 {
     operand_read(&opr_src);
     uint32_t temp=cpu.eflags.CF;
-    OPREAND t;
+    OPERAND t;
     t.val=0x1;
     opr_src.val = alu_add(opr_src.val,t.val,data_size);
     cpu.eflags.CF = temp;
