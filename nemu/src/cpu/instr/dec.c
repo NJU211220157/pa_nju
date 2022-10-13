c9f2433a134a39793a55dedf23f3a0338d93a716
tracer-pa-public <tracer@njuics.org>
2022-10-13 17:30:03 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:30:03 up 85 days,  8:56,  1 user,  load average: 2.10, 2.29, 2.34 > unitime 1665653403406 > 5db7d4f0932af200e5154fd3fec0882aff9d4ce4

--- nemu/src/cpu/instr/dec.c
+++ nemu/src/cpu/instr/dec.c
@@ -8,8 +8,7 @@ static void instr_execute_1op()
     operand_read(&opr_src);
     
     //uint32_t temp=cpu.eflags.CF;
-    //opr_src.val=alu_sub(opr_src.val,0x1,data_size);
-    opr_src.val-=1;
+    opr_src.val=alu_sub(0x1,opr_src.val,data_size);
     //cpu.eflags.CF =temp;
     
     operand_write(&opr_src);
