cf086e8e6718e9a3dd448c48ccdf186881b215ad
tracer-pa-public <tracer@njuics.org>
2022-10-13 11:29:20 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:29:20 up 85 days,  2:55,  1 user,  load average: 2.29, 2.20, 2.16 > unitime 1665631760293 > dadfa17bb5e2dad357eced87b72d35f75a2068df

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -2,3 +2,32 @@
 /*
 Put the implementations of `leave' instructions here.
 */
+
+make_instr_func(leave)
+{
+    OPERAND rbp,rsp;
+    
+    rbp.type=OPR_REG;
+    rbp.addr=0x101;
+    rbp.data_size=data_size;
+    operand_read(&rbp);
+    
+    rsp.type=OPR_REG;
+    rsp.addr=0x100;
+    rsp.data_size=data_size;
+    rsp.val=rbp.val;
+    operand_write(&rsp);
+    
+    
+    OPERAND rm;
+    
+    rm.type=OPR_MEM;
+    rm.addr=cpu.gpr[4].val;
+    rm.data_size=data_size;
+    operand_read(&rm);
+    
+    rbp.val=rm.val;
+    operand_write(&rbp);
+    
+    cpu.gpr[4].val += data_size/8;
+}
