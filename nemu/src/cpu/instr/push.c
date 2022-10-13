14310deee71de4728f4d0dda3fe57ba817a6be8c
tracer-pa-public <tracer@njuics.org>
2022-10-13 17:35:53 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 17:35:53 up 85 days,  9:02,  1 user,  load average: 1.90, 2.00, 2.19 > unitime 1665653753141 > e637a701bfd12f1dab49d7589e9fd90b9a20fa0d

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -18,17 +18,5 @@ static void instr_execute_1op(){
 }
 make_instr_impl_1op(push,r,v);
 make_instr_impl_1op(push,rm,v);
-// make_instr_func(push_r_v){
-//     uint8_t regIdx=opcode&07;//获取寄存器编号
-//     cpu.gpr[4].val-=data_size/8;
-//     OPERAND src;
-//     src.addr=cpu.gpr[regIdx].val;
-//     src.type=OPR_MEM;
-//     operand_read(&src);
-//     OPERAND dest;
-//     dest.addr=cpu.gpr[4].val;
-//     dest.type=OPR_MEM;
-//     dest.val=src.val;
-//     operand_write(&dest);
-//     return 1;//指令长度
-// }
+make_instr_impl_1op(push,i,b);
+make_instr_impl_1op(push,i,v);
