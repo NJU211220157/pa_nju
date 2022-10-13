7df7204506529da8370591691b9709cfebca4b26
tracer-pa-public <tracer@njuics.org>
2022-10-13 09:53:15 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 09:53:15 up 85 days,  1:19,  1 user,  load average: 2.19, 2.31, 2.27 > unitime 1665625995035 > a0721303e5f0434e1d90ae8a217c515e652d315a

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -25,7 +25,7 @@ make_instr_func(inc_rm_v)
     OPERAND rm;
     rm.data_size=data_size;
     int len=1;
-    len += modrm(eip+1,&rm);//寻址
+    len += modrm_rm(eip+1,&rm);//寻址
     operand_read(&rm);
     
     rm.val+=1;
