894912145d4934a8325370478982e14f9e0c08e8
tracer-pa-public <tracer@njuics.org>
2022-10-13 16:40:41 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:40:41 up 85 days,  8:07,  1 user,  load average: 2.50, 2.31, 2.29 > unitime > 6e6b1e53b6603b23f75c82d7a404117e836b8b1b

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,42 +2,43 @@
 /*
 Put the implementations of `inc' instructions here.
 */
-static void instr_execute_1op()
+// static void instr_execute_1op()
+// {
+//     operand_read(&opr_src);
+    
+//     OPERAND rm;
+//     rm.type=opr_src.type;
+//     rm.val=opr_src.val;
+    
+//     uint32_t t=cpu.eflags.CF;
+//     rm.data_size=data_size;
+    
+//     //mention!加的是0！
+//     cpu.eflags.CF=1;
+//     rm.val=alu_adc(rm.val,0,data_size);//
+    
+//     cpu.eflags.CF=t;//不改变CF
+    
+//     rm.addr=opr_src.addr;
+  
+//     //operand_write(&rm);
+//     //operand_write(&opr_dest);
+// }
+
+
+
+
+make_instr_func(inc_rm_v)
 {
-    operand_read(&opr_src);
-    
     OPERAND rm;
-    rm.type=opr_src.type;
-    rm.val=opr_src.val;
-    
-    uint32_t t=cpu.eflags.CF;
     rm.data_size=data_size;
     
-    //mention!加的是0！
-    cpu.eflags.CF=1;
-    rm.val=alu_adc(rm.val,0,data_size);//
+    int len=1;
+    len += modrm_rm(eip+1,&rm);//寻址
     
-    cpu.eflags.CF=t;//不改变CF
+    operand_read(&rm);
+    rm.val=alu_add(rm.val,0x1,data_size);
+    operand_write(&rm);
     
-    rm.addr=opr_src.addr;
-  
-    //operand_write(&rm);
-    //operand_write(&opr_dest);
+    return len;
 }
-
-
-make_instr_impl_1op(inc,rm,v);
-
-// make_instr_func(inc_rm_v)
-// {
-//     OPERAND rm;
-//     rm.data_size=data_size;
-//     int len=1;
-//     len += modrm_rm(eip+1,&rm);//寻址
-//     operand_read(&rm);
-    
-//     rm.val+=1;
-//     operand_write(&rm);
-    
-//     return len;
-// }
