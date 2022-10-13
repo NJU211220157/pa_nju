4b86114716c20ca2e189ea0e5ccd9ad4787f802b
tracer-pa-public <tracer@njuics.org>
2022-10-13 10:57:22 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:57:22 up 85 days,  2:23,  1 user,  load average: 2.43, 2.23, 2.16 > unitime 1665629842645 > 40bff2efbadc686e7675fad5c6510e2bf99467e5

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -5,6 +5,7 @@ Put the declarations of `add' instructions here.
 */
 make_instr_func(add_i2rm_b);
 make_instr_func(add_i2rm_v);
+make_instr_func(add_i2rm_bv);
 make_instr_func(add_i2a_b);
 make_instr_func(add_i2a_v);
 make_instr_func(add_r2rm_b);
