4ee4f8e7fa055ee6dd5ef936b819d329617833c0
tracer-pa-public <tracer@njuics.org>
2022-10-14 23:09:49 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:09:49 up 86 days, 14:36,  1 user,  load average: 2.11, 2.16, 2.13 > unitime > 30d407add81d017a71aaa7ea4824cdc5f727fd

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -13,10 +13,6 @@ static void instr_execute_2op(){
     else{
         alu_sub(opr_src.val,opr_dest.val,opr_dest.data_size);
     }
-    if(opr_src.val==0xffffff80&&opr_dest.val==0xffffff80){
-        printf("what");
-        cpu.eflags.ZF=1;
-    }
 }
 
 make_instr_impl_2op(cmp,rm,r,b);
