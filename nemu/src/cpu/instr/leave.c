6a302a211637d719371bffa4ea7f2e7d22a485bd
tracer-pa-public <tracer@njuics.org>
2022-10-14 11:32:11 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:32:11 up 86 days,  2:58,  1 user,  load average: 1.97, 2.11, 2.11 > unitime 1665718331341 > 14d9d6bb29d5b3e2afc475cbad41504d403ecd5b

--- nemu/src/cpu/instr/leave.c
+++ nemu/src/cpu/instr/leave.c
@@ -16,17 +16,20 @@ make_instr_func(leave)
     rsp.addr=4;
     rsp.data_size=data_size;
     rsp.val=rbp.val;
-    operand_write(&rsp);
     
     OPERAND rm;
     rm.type=OPR_MEM;
     rm.data_size=data_size;
     rm.addr=rsp.val;
+    //读取栈顶的值
     operand_read(&rm);
     
-    rbp.val=rm.val;
-    operand_write(&rbp);
     
+    rbp.val=rm.val;
+    
+    operand_write(&rsp);
+    operand_write(&rbp);
+
     cpu.gpr[4].val += data_size/8;
     
     return 1;
