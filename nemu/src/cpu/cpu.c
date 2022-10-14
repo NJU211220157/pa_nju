26451eabd30ac4130f3932b3db857049966f0fca
tracer-pa-public <tracer@njuics.org>
2022-10-14 11:02:41 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:02:41 up 86 days,  2:29,  1 user,  load average: 2.41, 2.28, 2.21 > unitime > 3a284a72e1e1420c74b80724a2bbc46afac8377c

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -63,8 +63,8 @@ void exec(uint32_t n)
 	{
 		if(!is_nemu_hlt)
 		{
-		    printf("eip=%x\n",cpu.eip);
-		    printf("eax = %x\n", cpu.gpr[0].val);
+		    //printf("eip=%x\n",cpu.eip);
+		    //printf("eax = %x\n", cpu.gpr[0].val);
 			instr_len = exec_inst();
 			cpu.eip += instr_len;
 			n--;
