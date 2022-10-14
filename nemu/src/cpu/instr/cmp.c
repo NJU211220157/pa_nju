8917f35d35e14a67534579a22624bf50f3d666cd
tracer-pa-public <tracer@njuics.org>
2022-10-14 16:53:48 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:53:48 up 86 days,  8:20,  1 user,  load average: 1.82, 2.21, 2.34 > unitime 1665737628105 > 2daf5f8bbf8cd73ac747975b436b9de41cd09c7

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -6,7 +6,9 @@ Put the implementations of `cmp' instructions here.
 static void instr_execute_2op(){
     operand_read(&opr_src);
     operand_read(&opr_dest);
-    opr_src.val=sign_ext(opr_src.val,opr_dest.data_size);
+    if(opr_src.data_size==8&&opr_src.type==OPR_IMM&&opr_dest.data_size>8){
+       opr_src.val=sign_ext(opr_src.val,opr_dest.data_size);
+    }
     alu_sub(opr_src.val,opr_dest.val,opr_dest.data_size);
 }
 
