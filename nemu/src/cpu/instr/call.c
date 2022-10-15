5dcc9a317ff954527bc2eafaa9ef1b0b7d22671d
tracer-pa-public <tracer@njuics.org>
2022-10-15 19:45:43 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:45:43 up 87 days, 11:12,  1 user,  load average: 2.50, 2.31, 2.30 > unitime 1665834343293 > edd201e861efd5751cbe7b421a65aa0a3c061f38

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -39,6 +39,84 @@ make_instr_func(call_near){
 }
 
 
+make_instr_func(call_near_indirect)
+{
+    
+    //push eip;
+    OPERAND rm;
+    cpu.gpr[4].val-=data_size/8;
+    
+    rm.type=OPR_MEM;
+    rm.addr=cpu.gpr[4].val;
+    rm.data_size=data_size;
+    
+    rm.val=cpu.eip+ 1 + (data_size/8);//important
+    //rm.val=cpu.eip;
+    
+    operand_write(&rm);
+
+    
+
+    OPERAND t;
+    t.type=OPR_MEM;
+    t.data_size=data_size;
+    int len=1;
+    len += modrm_rm(eip+1,&t);
+    operand_read(&t);
+    cpu.eip = t.val;
+    
+    if(data_size==16)
+    {
+        cpu.eip=cpu.eip&0x0000FFFF;
+    }
+    
+    return len;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
