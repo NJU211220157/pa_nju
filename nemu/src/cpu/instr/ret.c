dee6da9f7ba12a7aac3225562822a7bdbccdf36f
tracer-pa-public <tracer@njuics.org>
2022-10-15 11:12:31 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:12:31 up 87 days,  2:39,  2 users,  load average: 2.16, 2.38, 2.39 > unitime 1665803551473 > 69f982ba569a124cec3bfaed1964a96198350b2e

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -42,11 +42,11 @@ make_instr_func(ret_near_imm16)
     operand_read(&rm);
     
     cpu.eip=rm.val;
-    if(data_size==16)
-    {
-        cpu.eip=cpu.eip&0x0000FFFF;
-    }
-    cpu.gpr[4].val += 4;
+    // if(data_size==16)
+    // {
+    //     cpu.eip=cpu.eip&0x0000FFFF;
+    // }
+    cpu.gpr[4].val += data_size/8;
     cpu.gpr[4].val += imm.val;
     //cpu.gpr[4].val += sign_ext(imm.val,16);
     
