2db606423b820e72d54c8cd2a8290b9096bd4ee2
tracer-pa-public <tracer@njuics.org>
2022-10-15 20:03:08 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:03:08 up 87 days, 11:29,  1 user,  load average: 3.32, 2.58, 2.37 > unitime 1665835388744 > 7835dbb015d47e313909dff42a0c6900a643c299

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -50,7 +50,7 @@ make_instr_func(jmp_near_indirect)
         len += modrm_rm(eip+1,&rm);
         operand_read(&rm);
 
-        int offset = sign_ext(rm.val, rm.data_size);
+        //int offset = sign_ext(rm.val, rm.data_size);
         // thank Ting Xu from CS'17 for finding this bug
         print_asm_1("jmp", "", 1 + data_size / 8, &rm);
 
