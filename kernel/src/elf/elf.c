5aace225b33d5a8deebc8b70015d318cb329c716
tracer-pa-public <tracer@njuics.org>
2022-10-18 20:26:54 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:26:54 up 90 days, 11:53,  1 user,  load average: 2.76, 2.62, 2.46 > unitime 1666096014644 > 4173bc48db47a243d3da22e7d5fc918d9b2f46a8

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,6 +36,7 @@ uint32_t loader()
 	{
 		if (ph->p_type == PT_LOAD)
 		{
+		    panic("please!");
 /* TODO: copy the segment from the ELF file to its proper memory area */
             
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
