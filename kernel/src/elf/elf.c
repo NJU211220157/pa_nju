14e959f66e46c6654547f2612e0e929fe951fe3b
tracer-pa-public <tracer@njuics.org>
2022-10-18 19:48:23 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 19:48:23 up 90 days, 11:15,  1 user,  load average: 2.23, 2.13, 2.23 > unitime 1666093703784 > 22613f4b8a7aaa44fafad5d86608294a29840f6

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -30,20 +30,28 @@ uint32_t loader()
 #endif
 
 	/* Load each program segment */
-	ph = (void *)elf + elf->e_phoff;
-	eph = ph + elf->e_phnum;
+	ph = (void *)elf + elf->e_phoff;//初始地址
+	eph = ph + elf->e_phnum;//总共有这么多的ph
 	for (; ph < eph; ph++)
 	{
 		if (ph->p_type == PT_LOAD)
 		{
-
-			// remove this panic!!!
-			panic("Please implement the loader");
-
 /* TODO: copy the segment from the ELF file to its proper memory area */
-
+//如何将数据读取到内存当中？
+            Elf32_Addr src_mem_addr=ph->p_vaddr;
+            Elf32_Addr src_file_addr=(void *)elf + ph->p_offest;//从此处开始读取目标文件中的数据
+            Elf32_Addr dest_file_addr = src_file_addr + 8*ph->file_sz;
+            
+            uint32_t count=ph->p_filesz;//总共读这么读写这么多字节
+            
+            while(src_file_addr<dest_file_addr)
+            {
+                hw_mem_write(src_mem_addr, 1, (*src_file_addr));
+                src_mem_addr++;
+                src_file_addr++;
+            }
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
-
+            hw_mem_write(src_mem_addr, 8*(ph->p_memsz-count), 0);
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
 			extern uint32_t brk;
