1de2fa2fabca19c4dc550caced51627f31f08b15
tracer-pa-public <tracer@njuics.org>
2022-10-26 22:50:32 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 22:50:32 up 98 days, 14:17,  1 user,  load average: 2.26, 2.36, 2.31 > unitime 1666795832689 > c3ecb5f7791bb5e8704944194871a5171f9effdb

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -137,7 +137,7 @@ uint32_t eval(uint32_t p,uint32_t q){
         return -1;//返回一个不会对结果造成影响的值
     }
     else if(p == q){
-        if(tokens[p].token_type==NUM)
+        if(tokens[p].type==NUM)
             return atoi(tokens[p].str);
         else
             return -1;
@@ -194,6 +194,7 @@ uint32_t eval(uint32_t p,uint32_t q){
             default:assert(0);
         }
     }
+    return 0;
 }
 
 uint32_t check_parentheses(uint32_t p,uint32_t q){
