894ea5d63bae73acac1d30f20bfdec4fa8bd7c39
tracer-pa-public <tracer@njuics.org>
2022-10-26 23:32:59 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 23:32:59 up 98 days, 14:59,  1 user,  load average: 1.86, 2.15, 2.24 > unitime 1666798379147 > f6ef4c38719d70cc27b3d38be280bb770482910

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -179,6 +179,7 @@ uint32_t eval(uint32_t p,uint32_t q){
                     }
                     break;
                 }
+                default:
             }
             src++;
         }
