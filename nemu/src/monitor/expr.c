a6b687213f9d543b727d0af6d3fca3be18ccbd7c
tracer-pa-public <tracer@njuics.org>
2022-10-27 11:06:40 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:06:40 up 99 days,  2:33,  1 user,  load average: 2.06, 2.15, 2.20 > unitime 1666840000019 > 47c3e9b41c4766e34c77cf5d5dcc940dee137bc9

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -187,14 +187,14 @@ uint32_t eval(uint32_t p,uint32_t q){
             
             else { printf("fifa registers expr!\n"); return -1; }
         }
-        printf("fifa expr!\n");
+        printf("fifa expr in line190!\n");
         return -1;
     }
     else if(check_parentheses(p,q) == 1){
         return eval(p + 1, q - 1);
     }
     else if(check_parentheses(p,q) == -1){
-        printf("fifa expr!\n");
+        printf("fifa expr in line197!\n");
         return -1;
     }
     else{
