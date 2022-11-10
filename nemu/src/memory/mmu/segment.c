1224b87bc9c92667f6cba18d88b8484ae5ef0128
tracer-pa-public <tracer@njuics.org>
2022-11-10 11:10:45 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:10:45 up 113 days,  2:37,  1 user,  load average: 2.05, 2.27, 2.25 > unitime > 34ccdcb628d78a7ff2bc70d7997ddd6c51ca5f3

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -7,7 +7,8 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	return 0;
+	uint32_t base_ = cpu.segReg[sreg].base;
+	return offset + base_;
 }
 
 // load the invisible part of a segment register
@@ -16,4 +17,15 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+	 uint32_t index = cpu.segReg[sreg].index;
+	 SegDesc* segDesc = (void *)(cpu.gdtr.base + index * 8);//段表首地址
+	 uint32_t base1, base2, base3;
+	 base1 = segDesc->base_15_0;
+	 base2 = segDesc->base_23_16 << 16;
+	 base3 = segDesc->base_31_24 << 24;
+	 cpu.segReg[sreg].base = base1 + base2 + base3 ;
+	 cpu.segReg[sreg].limit = segDesc->limit_15_0 + (segReg->limit_19_16 << 16);
+	 cpu.segReg[sreg].type = segDesc->type;
+	 cpu.segReg[sreg].privilege_level = segDesc->privilege_level;
+	 cpu.segReg[sreg].soft_use = segDesc->soft_use;
 }
