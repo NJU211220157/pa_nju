7a6d149878594fc26abb14a017f18306f2d79194
tracer-pa-public <tracer@njuics.org>
2022-11-20 20:32:39 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:32:38 up 123 days, 11:59,  1 user,  load average: 2.57, 2.45, 2.35 > unitime 1668947558773 > 9ae5080c478fa07f3faa58dd2c8b5c553d356f4a

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -12,6 +12,8 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 }
 
 // load the invisible part of a segment register
+
+//按字节编址
 void load_sreg(uint8_t sreg)
 {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
@@ -20,19 +22,22 @@ void load_sreg(uint8_t sreg)
 	 uint32_t index = cpu.segReg[sreg].index;
 	 uint32_t addr = (cpu.gdtr.base + index * 64);
 	 
-	 uint32_t base1, base2;
-	 memcpy(&base1,(void *) (addr + 16),3);
-	 memcpy(&base2,(void *) (addr + 56),1);
+	 uint32_t base1 = 0, base2 = 0;
+	 memcpy(&base1,(void *) (addr + 2), 3);
+	 memcpy(&base2,(void *) (addr + 7), 1);
 	 cpu.segReg[sreg].base = base1  + (base2<<24) ;
 	 
-	 uint32_t limit1,limit2;
+	 uint32_t limit1 = 0,limit2 = 0;
 	 memcpy(&limit1,(void *) addr , 2);
-	 memcpy(&limit2,(void *) (addr + 48) , 1);
+	 memcpy(&limit2,(void *) (addr + 6) , 1);
 	 cpu.segReg[sreg].limit = limit1 + ((limit2<<28)>>12);
 	 
-	 uint32_t type1;
-	 memcpy(&type1,(void *)(addr + 40),1);
+	 uint32_t type1 = 0;
+	 memcpy(&type1,(void *)(addr + 5),1);
 	 cpu.segReg[sreg].type = type1 & 0xf;
+	 
+/*	 uint32_t privilege_level = 0;
+	 memcpy(&privilege_level,(void *)(addr + 45),);*/
 
 }
 
