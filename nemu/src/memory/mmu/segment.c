016b18bf4b6b3ab4bcd5772ce0bd89d57e4377dd
tracer-pa-public <tracer@njuics.org>
2022-11-20 20:50:33 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:50:33 up 123 days, 12:17,  1 user,  load average: 2.63, 2.54, 2.42 > unitime > 5a703c38692f9c6dd17eedb893a8f6a9f96b9be8

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -22,25 +22,28 @@ void load_sreg(uint8_t sreg)
 	 uint32_t index = cpu.segReg[sreg].index;
 	 uint32_t addr = (cpu.gdtr.base + index * 8);
 	 
-	 printf("addr = %d",addr);
 	 
 	 uint32_t base1 = 0, base2 = 0;
-	 memcpy(&base1,(void *) (addr + 2), 3);
-	 memcpy(&base2,(void *) (addr + 7), 1);
+	 memcpy(&base1, (void *) (addr + 2), 3);
+	 memcpy(&base2, (void *) (addr + 7), 1);
 	 cpu.segReg[sreg].base = base1  + (base2 << 24) ;
 	 
 	 uint32_t limit1 = 0,limit2 = 0;
-	 memcpy(&limit1,(void *) addr , 2);
-	 memcpy(&limit2,(void *) (addr + 6) , 1);
+	 memcpy(&limit1, (void *) addr , 2);
+	 memcpy(&limit2, (void *) (addr + 6) , 1);
 	 cpu.segReg[sreg].limit = limit1 + ((limit2 & 0xf) << 16);
 	 
 	 uint32_t type1 = 0;
-	 memcpy(&type1,(void *)(addr + 5),1);
+	 memcpy(&type1, (void *) (addr + 5),1);
 	 cpu.segReg[sreg].type = type1 & 0x1f;//取低五位
-/*	 
-	 uint32_t privilege_level = 0;
-	 memcpy(&privilege_level,(void *)(addr + 45),);*/
+	 
+	 uint32_t privilege_level1 = 0;
+	 memcpy(&privilege_level1,(void *)(addr + 5),1);
+	 cpu.segReg[sreg].privilege_level = privilege_level1 & 0x60;
 
+     uint32_t soft_use1 = 0;
+     memcpy(&soft_use1, (void *) (addr + 6), 1);
+     cpu.segReg[sreg].soft_use = soft_use1 & 0x10;  
 }
 
 
