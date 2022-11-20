506a01b93a2cb5b7db5a7ea99889c792f665739e
tracer-pa-public <tracer@njuics.org>
2022-11-20 20:34:10 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 20:34:10 up 123 days, 12:00,  1 user,  load average: 2.37, 2.39, 2.34 > unitime > a1016cf64aba755553287cfa94e757dfd478b96c

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -7,7 +7,6 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	return 0;
 	uint32_t base_ = cpu.segReg[sreg].base;
 	return offset + base_;
 }
@@ -20,6 +19,7 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
+	 return;
 	 uint32_t index = cpu.segReg[sreg].index;
 	 uint32_t addr = (cpu.gdtr.base + index * 64);
 	 
