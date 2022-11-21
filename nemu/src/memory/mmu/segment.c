11230f5f9004179da57ede713f39a59692cc485f
tracer-pa-public <tracer@njuics.org>
2022-11-21 16:00:37 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:00:37 up 124 days,  7:27,  1 user,  load average: 2.13, 2.32, 2.33 > unitime > 24935b1f2af2f5b3e9c2e714600c574c82c298b

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -20,24 +20,14 @@ void load_sreg(uint8_t sreg)
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-
-	 
 	 SegDesc segdesc;
-	 
      segdesc.val[0] = hw_mem_read(cpu.gdtr.base + cpu.segReg[sreg].index * sizeof(SegDesc),4);
      segdesc.val[1] = hw_mem_read(cpu.gdtr.base + cpu.segReg[sreg].index * sizeof(SegDesc) + 4,4);
 	 assert(segdesc.present == 1);
 	 
-	 uint32_t base1 = 0, base2 = 0,base3 = 0;
-	 base1 = segdesc.base_15_0;
-	 base2 = segdesc.base_23_16;
-	 base3 = segdesc.base_31_24;
-	 cpu.segReg[sreg].base = base1 + (base2 << 16) + (base3 << 24) ;
+	 cpu.segRegisregl.base = segdesc.base_15_0 + (segdesc.base_23_16 << 16) + (segdesc.base_31_24 << 24);
 	 
-	 uint32_t limit1 = 0,limit2 = 0;
-	 limit1 = segdesc.limit_15_0;
-	 limit2 = segdesc.limit_19_16;
-	 cpu.segReg[sreg].limit = limit1 + (limit2 << 16);
+	 cpu.segReg[sregl.limit = segdesc.limit_15_0  + (segdesc.limit_19_16 << 16);
 	 
 
 }
