cc9e5aa7a7041a77fc8b8c65b96bdd6c40afd3a6
tracer-pa-public <tracer@njuics.org>
2022-11-21 16:12:45 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:12:45 up 124 days,  7:39,  1 user,  load average: 2.38, 2.25, 2.27 > unitime 1669018365242 > b4252fc7ee71fe68037a5dbd3bba00fa4acb3b5

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -8,10 +8,10 @@ make_instr_func(lgdt)
     uint32_t rm1,rm2;
     uint32_t addr = cpu.eip + 2;
     
-    memcpy(&rm1,(void*)addr,2);
+    memcpy(&rm1,(void*) addr, 2);
     memcpy(&rm2,(void*)(addr + 2),4);
     cpu.gdtr.limit = rm1;
     cpu.gdtr.base = rm2;
     
-    return 6;
+    return 7;
 }
