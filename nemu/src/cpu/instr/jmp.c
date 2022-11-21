e01c8ec6117ca6b7bf3870a08917130825cb6c46
tracer-pa-public <tracer@njuics.org>
2022-11-21 16:31:45 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 16:31:45 up 124 days,  7:58,  1 user,  load average: 2.00, 2.17, 2.23 > unitime 1669019505019 > a79f7fb6e11d402a1fba689940312cee1dfd558f

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -1,5 +1,30 @@
 #include "cpu/instr.h"
 
+void load_sreg(uint8_t sreg);
+
+
+make_instr_func(jmp_far_imm)
+{
+    OPERAND rel;
+    data_size = 32;
+    load_sreg(1);
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = data_size;
+    rel.addr = eip + 1;
+
+    operand_read(&rel);
+
+    int offset = sign_ext(rel.val, rel.data_size);
+        // thank Ting Xu from CS'17 for finding this bug
+    print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+
+    cpu.eip += offset;
+
+    return 1 + data_size / 8;
+}
+
+
 make_instr_func(jmp_near)
 {
         OPERAND rel;
@@ -59,8 +84,6 @@ make_instr_func(jmp_near_indirect)
 
         return 0;
 }
-
-
 
 
 
