db98c9a6aec9023bfc7002ed4858af624f824086
tracer-pa-public <tracer@njuics.org>
2022-11-21 15:51:11 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 15:51:11 up 124 days,  7:17,  1 user,  load average: 2.30, 2.33, 2.28 > unitime > 94db651c50da9667f4b27843a2527a73a6023d7f

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -24,13 +24,13 @@ void load_sreg(uint8_t sreg)
 	 uint32_t addr = (cpu.gdtr.base + index2 * 8);
 	 
 	 SegDesc segdesc;
-     memcpy(&segdesc,(void *)addr,8);
+     memcpy(&segdesc, (void *)addr, 8);
 	 
 	 uint32_t base1 = 0, base2 = 0,base3 = 0;
 	 base1 = segdesc.base_15_0;
 	 base2 = segdesc.base_23_16;
 	 base3 = segdesc.base_31_24;
-	 cpu.segReg[sreg].base = base1  + + (base2 << 16) + (base3 << 24) ;
+	 cpu.segReg[sreg].base = base1 + (base2 << 16) + (base3 << 24) ;
 	 
 	 uint32_t limit1 = 0,limit2 = 0;
 	 limit1 = segdesc.limit_15_0;
