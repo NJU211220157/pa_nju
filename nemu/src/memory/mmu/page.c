a28e91000a657367333c64ca3551b27807ce56dc
tracer-pa-public <tracer@njuics.org>
2022-12-03 10:35:06 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 10:35:06 up 136 days,  2:01,  1 user,  load average: 2.38, 2.46, 2.40 > unitime > 4797d16ad42af9fd5e1a6983f35e2ac8b0aef209

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,20 +9,20 @@ paddr_t page_translate(laddr_t laddr)
 #ifndef TLB_ENABLED
     //页目录和页表项的物理页需要偏移12位吗
     //return 0 甚至得到一样的结果
-	uint32_t dir = laddr >> 22;//只有高十位
-	uint32_t page = (laddr << 10 ) >> 22;
-	uint32_t offset = laddr & 0xfff;
+	uint32_t dir_ = laddr >> 22;//只有高10位
+	uint32_t page_ = (laddr << 10 ) >> 22;//取中间10位
+	uint32_t offset_ = laddr & 0xFFF;//低12位
 	
 	PDE pde;
-    pde.val = hw_mem_read((cpu.cr3.base) + dir * 4, 4);
+    pde.val = hw_mem_read((cpu.cr3.base) + dir_ * sizeof(PDE), sizeof(PDE));
     assert(pde.present == 1);
     
 	PTE pte;
-	pte.val = hw_mem_read((pde.page_frame) + page * 4, 4);
+	pte.val = hw_mem_read((pde.page_frame) + page_ * sizeof(PTE), sizeof(PTE));
 	assert(pte.present == 1);
 	
 	uint32_t p_page = pte.page_frame << 12;
-	return p_page + offset;
+	return p_page + offset_;
 	
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
