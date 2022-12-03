dc2b8e057ed6f517cfa268d9d792dfdecae8be94
tracer-pa-public <tracer@njuics.org>
2022-12-03 11:32:15 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:32:15 up 136 days,  2:58,  1 user,  load average: 2.38, 2.37, 2.39 > unitime > ba398f5141f293e25e76900222fa81ca5bf04c4d

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,16 +9,16 @@ paddr_t page_translate(laddr_t laddr)
 #ifndef TLB_ENABLED
     //页目录和页表项的物理页需要偏移12位吗
     //return 0 甚至得到一样的结果
-	uint32_t dir_ = laddr >> 22;//只有高10位
-	uint32_t page_ = (laddr << 10 ) >> 22;//取中间10位
+	uint32_t dir_ = (laddr >> 22) & 0x3FF;//只有高10位
+	uint32_t page_ = ((laddr << 10 ) >> 22) & 0x3FF;//取中间10位
 	uint32_t offset_ = laddr & 0xFFF;//低12位
 	
 	PDE pde;
-    pde.val = hw_mem_read( cpu.cr3.base + dir_ * sizeof(PDE), sizeof(PDE));
-    //assert(pde.present == 1);
+    pde.val = hw_mem_read( (cpu.cr3.base << 12) + dir_ * sizeof(PDE), sizeof(PDE));
+    assert(pde.present == 1);
     
 	PTE pte;
-	pte.val = hw_mem_read( pde.page_frame + page_ * sizeof(PTE), sizeof(PTE));
+	pte.val = hw_mem_read( (pde.page_frame << 12) + page_ * sizeof(PTE), sizeof(PTE));
 	assert(pte.present == 1);
 	
 	uint32_t p_page = pte.page_frame << 12;
