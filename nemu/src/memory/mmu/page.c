14460863a9a071fd84f599aede0252fddb0a03ab
tracer-pa-public <tracer@njuics.org>
2022-12-03 11:33:29 +0800

    > compile nemu testcase kernel > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:33:29 up 136 days,  3:00,  1 user,  load average: 2.35, 2.36, 2.38 > unitime 1670038409347 > c4f2b6a26a783628cb924afc43c4174e2abc5d82

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,6 +9,13 @@ paddr_t page_translate(laddr_t laddr)
 #ifndef TLB_ENABLED
     //页目录和页表项的物理页需要偏移12位吗
     //return 0 甚至得到一样的结果
+    PDE* p1 = (PDE*) (hw_mem + (cpu.cr3.base << 12) + 4 * ((laddr >> 22) & 0x000003FF));
+	assert(p1->present == 1);
+	PTE* p2 = (PTE*) (hw_mem + (p1->page_frame << 12) + 4 * ((laddr >> 12) & 0x000003FF));
+	assert(p2->present == 1);
+	uint32_t paddr = (p2->page_frame << 12) + (laddr & 0x00000FFF);
+	return paddr;
+	/*
 	uint32_t dir_ = (laddr >> 22) & 0x3FF;//只有高10位
 	uint32_t page_ = ((laddr << 10 ) >> 22) & 0x3FF;//取中间10位
 	uint32_t offset_ = laddr & 0xFFF;//低12位
@@ -23,7 +30,7 @@ paddr_t page_translate(laddr_t laddr)
 	
 	uint32_t p_page = pte.page_frame << 12;
 	return p_page + offset_;
-	
+	*/
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 #endif
