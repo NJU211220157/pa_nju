7b5c49617413a8fd6b96a9fa61948151a9e404d3
tracer-pa-public <tracer@njuics.org>
2022-12-04 11:11:36 +0800

    > clean > id 211220157 > user pa211220157 > uname Linux 2189b12235fd 4.15.0-45-generic #48~16.04.1-Ubuntu SMP Tue Jan 29 18:03:48 UTC 2019 x86_64 GNU/Linux > uptime 11:11:36 up 137 days,  2:38,  1 user,  load average: 2.00, 2.23, 2.25 > unitime > ba6599744aed3ebfdc432da27e7bbbb3555e5090

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -42,7 +42,10 @@ void paddr_write(paddr_t paddr, size_t len, uint32_t data)
 
 uint32_t laddr_read(laddr_t laddr, size_t len)
 {
-    if(cpu.cr0.pg == 1 && cpu.cr0.pe == 1){
+#ifndef IA32_PAGE
+	return paddr_read(laddr, len);
+#else
+    if(cpu.cr0.pg == 1){
         if((laddr % 0x1000 + len) > 0x1000){
             uint32_t paddr = page_translate(laddr);
             return paddr_read(paddr, len);
@@ -64,11 +67,15 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
     }
     else
 	    return paddr_read(laddr, len);
+#endif
 }
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 {
-	if(cpu.cr0.pg == 1 && cpu.cr0.pe == 1){
+#ifndef IA32_PAGE
+	paddr_write(laddr, len, data);
+#else
+	if(cpu.cr0.pg == 1){
         if((laddr % 0x1000 + len) > 0x1000){
             uint32_t paddr = page_translate(laddr);
             paddr_write(paddr, len ,data);
@@ -91,6 +98,7 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data)
     }
     else
 	    paddr_write(laddr, len , data);
+#endif
 }
 
 uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
